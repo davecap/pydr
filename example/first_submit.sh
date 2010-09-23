b93d0badcfd606de5fda4f78795d14ea36821fbd
David Caplan <dacaplan@scinet01.(none)>
2010-09-23 17:51:51 -0400

    update the example

--- example/first_submit.sh
+++ example/first_submit.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#PBS -l nodes=4:ib:ppn=8,walltime=10:00:00,os=centos53computeA
-#PBS -N testpydr
-
-MPIFLAGS="-mca btl_sm_num_fifos 7 -mca mpi_paffinity_alone 1 -mca btl_openib_eager_limit 32767 -np $(wc -l $PBS_NODEFILE | gawk '{print $1}') -machinefile $PBS_NODEFILE"
+#PBS -l nodes=4:ib:ppn=8,walltime=46:00:00,os=centos53computeA
+#PBS -N 1m56wt0
 
 cd $PBS_O_WORKDIR
 
@@ -11,4 +9,5 @@ module load python
 module load hdf5/184-p1-v18-serial
 source ~/ENV/bin/activate
 
-python /home/dacaplan/projects/pydr/pydr.py -j $PBS_JOBID -s
+python /home/dacaplan/projects/pydr/pydr.py -j $PBS_JOBID --pbs-nodefile $PBS_NODEFILE -s >> logs/$PBS_JOBID.log 2>&1
+
--- example/run.sh
+++ example/run.sh
@@ -8,14 +8,22 @@
 #   $sequence
 #   $coordinate
 #   $k
+#   $pdb
+#   $PBS_NODEFILE
+#   $PBS_O_WORKDIR
+#   $PBS_JOBID
 
 BASE_DIR=`pwd`
 SHM_DIR=/dev/shm/dacaplan
 MPIRUN=/scinet/gpc/mpi/openmpi/1.4.1-intel-v11.0-ofed/bin/mpirun
 NAMD=~/bin/namd2
 
+PDB=$BASE_DIR/$pdb
+
+MPIFLAGS="-mca btl_sm_num_fifos 7 -mca mpi_paffinity_alone 1 -mca btl_openib_eager_limit 32767 -np $(wc -l $PBS_NODEFILE | gawk '{print $1}') -machinefile $PBS_NODEFILE"
 export PATH=/home/dacaplan/ENV/bin:/home/dacaplan/bin:/project/pomes/dacaplan/gromacs/gromacs-git/exec/bin:/scinet/gpc/compilers/gcc/bin/:/usr/lib64/qt-3.3/bin:/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/usr/lpp/mmfs/bin:/opt/torque/bin:/opt/torque/sbin:/usr/lpp/mmfs/bin:/opt/torque/bin:/opt/torque/sbin:/scinet/gpc/intel/Compiler/11.1/056/bin/intel64/:/scinet/gpc/mpi/openmpi/1.4.1-intel-v11.0-ofed/bin/:$PATH
 export LD_LIBRARY_PATH=/scinet/gpc/tools/Python/Python262/lib:/project/pomes/dacaplan/gromacs/gromacs-git/exec/lib:/scinet/gpc/lib/mpfr/lib:/scinet/gpc/compilers/gcc/lib64:/scinet/gpc/compilers/gcc/lib:/scinet/gpc/intel/Compiler/11.1/056/lib/intel64/:/scinet/gpc/intel/Compiler/11.1/056/mkl/lib/em64t/:/scinet/gpc/mpi/openmpi/1.4.1-intel-v11.0-ofed/lib:/scinet/gpc/mpi/openmpi/1.4.1-intel-v11.0-ofed/lib/openmpi:$LD_LIBRARY_PATH
+export PYTHONPATH=/home/dacaplan/lib/python:/home/dacaplan/lib/python2.6/site-packages:/home/dacaplan/lib/python2.4/site-packages:/scinet/gpc/tools/Python/Python262/lib/python2.6/:/scinet/gpc/tools/Python/Python262/lib/python2.6/lib-dynload/:/scinet/gpc/tools/Python/Python262/lib/python2.6/site-packages/:/scinet/gpc/tools/Python/Python262/lib/python2.6/site-packages/Scientific/linux2/:/scinet/gpc/tools/Python/Python262/lib/python2.6/lib-tk/
 
 # Create the replica/sequence dir in the current directory (the dir containing this file)
 OUTPUT_DIR=$BASE_DIR/$id/$sequence
@@ -37,7 +45,6 @@ cd $SHM_DIR
 # depending on the sequence number, use the correct input files
 if [ "$sequence" == "0" ]; then
     CONF=md.conf
-    cp $OUTPUT_DIR/../md.pdb* $SHM_DIR/
 else
     # setup the restart files if sequence > 0
     CONF=md.restart.conf
@@ -46,33 +53,60 @@ else
     echo "Using restart data from previous run: $previous"
     cp $OUTPUT_DIR/../$previous/restart.tar.gz $SHM_DIR/
     tar xzvf $SHM_DIR/restart.tar.gz
+    rm $SHM_DIR/restart.tar.gz
 fi
+    
+cp $PDB $SHM_DIR/md.pdb.gz
+gunzip $SHM_DIR/md.pdb.gz
 
 cp $BASE_DIR/md.psf $SHM_DIR/ 
 # Generate the input files using the environment variables
 cp $BASE_DIR/tclforces.tcl $SHM_DIR/
 cat $BASE_DIR/$CONF | sed -e "s/__CHI1_K__/$k/" | sed -e "s/__CHI1_COORD__/$coordinate/" > $SHM_DIR/$CONF
 
+RETURN_CODE=0
+
+function analysis {
+    ANALYSIS=/home/dacaplan/projects/labwork/mdanalysis/cco_analysis.py
+    . /etc/bashrc
+    module purge
+    module load gcc/gcc-4.4.0
+    module load python
+    module load hdf5/184-p1-v18-serial
+    source /home/dacaplan/ENV/bin/activate
+    python $ANALYSIS -o $OUTPUT_DIR/../analysis.h5 md.psf md.pdb md.dcd
+}
+
 function finish {
     printf "Saving results from directory $SHM_DIR to $OUTPUT_DIR on "; date
-    tar zcf $OUTPUT_DIR/restart.tar.gz restart.*
     gzip md.log
     mv md.log.gz $OUTPUT_DIR/
-    mv $SHM_DIR/md.dcd $OUTPUT_DIR/md.dcd
     mv $SHM_DIR/md.*.conf $OUTPUT_DIR/
+
+    if [ -e  "restart.coor" ]; then
+        rm $SHM_DIR/restart.*.old
+        tar zcf $OUTPUT_DIR/restart.tar.gz restart.*
+        if [ -e "md.dcd" ]; then
+            analysis
+        fi
+        #mv $SHM_DIR/md.dcd $OUTPUT_DIR/md.dcd
+    else
+        echo "No restart file! run must have crashed"
+        RETURN_CODE=1
+    fi
+
     printf "Saving results complete on "; date
     printf "Cleaning up temporary directory $SHM_DIR at "; date
     rm -rf $SHM_DIR
     printf "done at "; date
 }
 
-# function trap_term {
-#     printf "Trapped term (soft kill) signal on "; date
-#     finish && exit 0
-# }
-#
-# TODO: catch the term signal (propagate it through the client)
-# trap "trap_term" TERM
+function trap_term {
+    printf "Trapped term (soft kill) signal on "; date
+    finish && exit $RETURN_CODE
+}
+
+trap "trap_term" TERM
 
 printf "Starting run at "; date
 echo "Running: $MPIRUN $MPIFLAGS $NAMD $SHM_DIR/$CONF >> $SHM_DIR/md.log"
@@ -85,5 +119,5 @@ printf "Run ended cleanly at "; date
 
 # TODO: run analysis (eg: extract dihedrals)
 
-finish && exit 0
+finish && exit $RETURN_CODE
 
